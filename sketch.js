f7b51faf8608347a25b9c012c99f2c31f38aa788
viktorshamal <online@viktorshamal.dk>
2023-02-14 15:19:46 +0100

    lmao

--- sketch.js
+++ sketch.js
@@ -1,24 +1,39 @@
-const { random, floor } = Math;
+const {
+  random,
+  floor
+} = Math;
 
 const randomBetween = (min, max) => random() * (max - min) + min;
 
-const drawing = p => {
-  p.setup = () => p.createCanvas(800, 800)
+const defaultCanvas = (p, node) => {
+  p.createCanvas(600, 600);
+  p.fill("black");
+  p.text(node.id, 10, p.height - 30);
+}
 
-  p.draw = () => {
-    let ellipseWidth = 60;
-    let ellipseHeight = ellipseWidth;
-    
-    p.noStroke();
-    p.fill(`#${floor(random()*16777215).toString(16)}`);
-    
-    if (p.mouseIsPressed) {
-      ellipseWidth = randomBetween(ellipseWidth / 1.2, ellipseWidth * 1.2);
-      ellipseHeight = ellipseWidth;
+const drawings = {
+  "view": node => p => {
+    p.setup = () => defaultCanvas(p, node);
+
+    p.draw = () => {
+      let ellipseWidth = 60;
+      let ellipseHeight = ellipseWidth;
+
+      p.noStroke();
+      p.fill(`#${floor(random()*16777215).toString(16)}`);
+
+      if (p.mouseIsPressed) {
+        ellipseWidth = randomBetween(ellipseWidth / 1.2, ellipseWidth * 1.2);
+        ellipseHeight = ellipseWidth;
+      }
+
+      p.ellipse(p.mouseX, p.mouseY, ellipseWidth, ellipseHeight);
     }
-    
-    p.ellipse(p.mouseX, p.mouseY, ellipseWidth, ellipseHeight);
   }
 }
 
-[...document.querySelectorAll('.canvas')].map(node => { new p5(drawing, node.id) })
+const testElements = document.querySelectorAll("[component]");
+
+Array.prototype.forEach.call(testElements, function(node) {
+  new p5(drawings[node.getAttribute("component")](node), node.id)
+})
