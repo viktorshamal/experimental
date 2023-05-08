122940593e4932f0ce40be68cc7dff860a8df9a8
viktorshamal <online@viktorshamal.dk>
2023-05-08 14:02:13 +0200

    Shaking disco!

--- libraries/itu_mqtt_config.js
+++ libraries/itu_mqtt_config.js
@@ -1,5 +1,5 @@
 // --- SETTING UP --------------------------------------
-//const myBroker = "wss://edp21:Ko5z2bU0Uf7ajNzv@edp21.cloud.shiftr.io"; 
+//const myBroker = "wss://edp21:Ko5z2bU0Uf7ajNzv@edp21.cloud.shiftr.io";
 const myBroker = "wss://edp22:JlTwNIbtNIwZk0IA@edp22.cloud.shiftr.io";
 
 const myID = "itu" + parseInt(Math.random() * 10000000, 10);
@@ -8,7 +8,7 @@ const client = mqtt.connect(myBroker, {clientId: myID});
 // --- CONNECTING--------------------------------------
 client.on('connect', function() {
   console.log('connected!');
-  client.subscribe(myTopic);
+  client.subscribe(myTopic)
   console.log(myTopic)
 });
 
