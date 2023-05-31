0c571f491ff64f13a2301fb5fd4fda902188ec28
viktorshamal <online@viktorshamal.dk>
2023-05-31 10:23:39 +0200

    prettier

--- libraries/motion.js
+++ libraries/motion.js
@@ -6,84 +6,61 @@ var sensor = {
   z: 0,
   difference: 0,
   threshold: 0,
-  hasNewValue: false
+  hasNewValue: false,
 };
 
-var accX=0;
-var accY=0;
-var accZ=0;
-var accXOld=0;
-var accYOld=0;
-var accZOld=0;
+var accX = 0;
+var accY = 0;
+var accZ = 0;
+var accXOld = 0;
+var accYOld = 0;
+var accZOld = 0;
 
 var faktor = 100;
 
+function doSensor(e) {
+  accX = Math.round(faktor * e.acceleration.x);
+  accY = Math.round(faktor * e.acceleration.y);
+  accZ = Math.round(faktor * e.acceleration.z);
 
-function doSensor(e)
-  {
-     accX = Math.round(faktor*e.acceleration.x);
-     accY = Math.round(faktor*e.acceleration.y);
-     accZ = Math.round(faktor*e.acceleration.z);
+  sensor.difference = distance3D(accX, accY, accZ, accXOld, accYOld, accZOld);
 
-    sensor.difference =  distance3D(accX,accY,accZ, accXOld,accYOld,accZOld);
+  if (sensor.difference > sensor.threshold) {
+    // console.log("difference: ", difference);
+    sensor.x = accX;
+    sensor.y = accY;
+    sensor.z = accZ;
+    sensor.hasNewValue = true;
 
-document.getElementById("showdata").innerHTML=sensor.difference;
-
-    if (sensor.difference > sensor.threshold)
-    {
-       // console.log("difference: ", difference);
-        sensor.x = accX;
-        sensor.y = accY;
-        sensor.z =accZ;
-        sensor.hasNewValue = true;
-
-        accXOld=accX;
-        accYOld=accY;
-        accZOld=accZ;
-    }
-  else
-    {
-        sensor.hasNewValue = false;
-    }
-
+    accXOld = accX;
+    accYOld = accY;
+    accZOld = accZ;
+  } else {
+    sensor.hasNewValue = false;
+  }
 }
-
 
 //-----------3D distance used to calculate difference between new and old values ---------
- function distance3D(xNew,yNew,zNew,xOld,yOld,zOld)
-{
-    let a = xNew - xOld;
-    let b = yNew - yOld;
-    let c = zNew - zOld;
+function distance3D(xNew, yNew, zNew, xOld, yOld, zOld) {
+  let a = xNew - xOld;
+  let b = yNew - yOld;
+  let c = zNew - zOld;
 
-    let afstand = Math.round(Math.sqrt(a * a + b*b + c*c));
-    return afstand;
+  let afstand = Math.round(Math.sqrt(a * a + b * b + c * c));
+  return afstand;
 }
 
-
 //----------------------------REQUEST PERMISSION ON IOS DEVICeS --------------------------
-function touchStarted()
-{
-
-  if (typeof DeviceMotionEvent.requestPermission === 'function')
-  {
-      DeviceMotionEvent.requestPermission()
-            .then(permissionState => {
-      if (permissionState === 'granted')
-        {
-                    window.addEventListener("devicemotion", doSensor, false);
-                }
-              })
-            .catch(console.error);
-     }
-
-     else
-  {
+function touchStarted() {
+  if (typeof DeviceMotionEvent.requestPermission === "function") {
+    DeviceMotionEvent.requestPermission()
+      .then((permissionState) => {
+        if (permissionState === "granted") {
+          window.addEventListener("devicemotion", doSensor, false);
+        }
+      })
+      .catch(console.error);
+  } else {
     window.addEventListener("devicemotion", doSensor, false);
   }
-
-
-
-
-
 }
